6774976a7899d0dfaabfa13400c04b4f004d689c
peter <student@epicodus.com>
2014-04-16 10:42:26 -0700

    adds making post to user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,18 @@ class PostsController < ApplicationController
 
   end
 
+  def new
+    if user_signed_in?
+      @post = Post.new
+    else
+      redirect_to "/users/sign_in"
+    end
+  end
+
+  def create
+    @user = User.find(current_user.id)
+    @user.posts.create(params.require(:post).permit(:title, :description))
+    redirect_to "/"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts
+  resources :users do
+    resources :posts
+  end
   root to: "posts#index"
 end
