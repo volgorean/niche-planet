c03b439319303f8118ef6ef86a9cb996446c4818
peter <volgorean@gmail.com>
2014-04-18 09:57:22 -0700

    adds map and itinerary to itinerary maker

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -6,7 +6,16 @@ class ItinerariesController < ApplicationController
     if current_user.id == params[:user_id].to_i
       @user = User.find(params[:user_id])
       @post = Post.find(params[:post_id])
+      @itineraries = @post.itineraries
       @itinerary = @post.itineraries.new
+      @coords = []
+      @coord = []
+      @itineraries.each do |itinerary|
+        if itinerary.lat && itinerary.lng
+          @coord << {lat: itinerary.lat, lng: itinerary.lng}
+        end
+      end
+      @coords << @coord
     else
       redirect_to "/"
     end
@@ -15,7 +24,7 @@ class ItinerariesController < ApplicationController
   def create
     @post = Post.find( itinerary_params[:post_id] )
     @post.itineraries.create( itinerary_params )
-    redirect_to "/posts/#{@post.id}/edit"
+    redirect_to "/users/#{current_user.id}/posts/#{@post.id}/itineraries/new"
   end
 
 private
