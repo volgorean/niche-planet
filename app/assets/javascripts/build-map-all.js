4401c08e0d3ea356e45cf02af772310d5b89e3e5
peter <volgorean@gmail.com>
2014-04-18 10:08:59 -0700

    fixes map error where coordinates overwritten

--- app/assets/javascripts/build-map-all.js
+++ app/assets/javascripts/build-map-all.js
@@ -1,28 +1,54 @@
 
 $( document ).ready(function() {
-  var coords = $('#multi_overlays').data('coords');
-
   var handler = Gmaps.build('Google');
-  handler.buildMap({ internal: {id: 'multi_overlays'}}, function(){
-    coords.forEach(function(coord){
-      if (coord.length > 1) {
-        var polylines1 = handler.addPolylines(
-          [
-            coord
-          ],
-          { strokeColor: '#9E1617'}
-        );
-        handler.bounds.extendWith(polylines1);
 
-      } else if (coord.length == 1) {
-        var circles = handler.addCircles(
-            [{ lat: coord[0].lat, lng: coord[0].lng, radius: 5000 }],
+  if(document.getElementById("multi_overlays")!=null) {
+    var coords = $('#multi_overlays').data('coords');
+    handler.buildMap({ internal: {id: 'multi_overlays'}}, function(){
+      coords.forEach(function(coord){
+        if (coord.length > 1) {
+          var polylines1 = handler.addPolylines(
+            [
+              coord
+            ],
+            { strokeColor: '#9E1617'}
+          );
+          handler.bounds.extendWith(polylines1);
 
-            { strokeColor: '#FF0000'}
-        );
-        handler.bounds.extendWith(circles);
-      }
+        } else if (coord.length == 1) {
+          var circles = handler.addCircles(
+              [{ lat: coord[0].lat, lng: coord[0].lng, radius: 5000 }],
+
+              { strokeColor: '#FF0000'}
+          );
+          handler.bounds.extendWith(circles);
+        }
+      });
+      handler.fitMapToBounds();
     });
-    handler.fitMapToBounds();
-  });
+  }else if (document.getElementById("multi_overlays_with_picker")!=null) {
+    var coords = $('#multi_overlays_with_picker').data('coords');
+    handler.buildMap({ internal: {id: 'multi_overlays_with_picker'}}, function(){
+      coords.forEach(function(coord){
+        if (coord.length > 1) {
+          var polylines1 = handler.addPolylines(
+            [
+              coord
+            ],
+            { strokeColor: '#9E1617'}
+          );
+          handler.bounds.extendWith(polylines1);
+
+        } else if (coord.length == 1) {
+          var circles = handler.addCircles(
+              [{ lat: coord[0].lat, lng: coord[0].lng, radius: 5000 }],
+
+              { strokeColor: '#FF0000'}
+          );
+          handler.bounds.extendWith(circles);
+        }
+      });
+      handler.fitMapToBounds();
+    });
+  }
 });
